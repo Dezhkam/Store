4a29178a0d3fd92d59ac7f11da62e3ae9e548696
Rasool Dezhkam <Dezhkam@yahoo.com>
2023-05-27 16:30:49 +0330

    Start coding ImageToolsService End 250

---
+++ app/Http/Services/Image/ImageToolsService.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Services\Image;
+
+class ImageToolsService{
+    protected $image;
+    protected $exclusiveDirectory;
+    protected $imageDirectory;
+    protected $imageName;
+    protected $imageFormat;
+    protected $finalImageDirectory;
+    protected $finalImageName;
+
+}
